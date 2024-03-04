4eeab0d1650d06e00a41649dfd7a839153f600ad
Ninty <ninty0808@gmail.com>
2024-03-04 11:24:35 +0100

    calender buttons and date

--- src/app/calender/page.js
+++ src/app/calender/page.js
@@ -2,17 +2,18 @@
 import {useState} from "react";
 import Calendar, {MonthView} from "react-calendar";
 import "../globals.css"
-import "./calender.css"
 
 export default function Calender() {
-    const [value, onChange] = useState(new Date());
     const [events, setEvents] = useState([
         new event(1, 'event 1', new Date(2024, 1, 1, 11), new Date(2024, 1, 3, 14, 30), null, 'description', 'catagory', 'location'),
         new event(2, 'event 2', new Date(2024, 1, 24, 11), new Date(2024, 1, 25, 14, 30), null, 'description', 'catagory', 'location'),
         new event(1, 'event 3', new Date(2024, 2, 7, 11), new Date(2024, 2, 7, 14, 30), null, 'description', 'catagory', 'location'),
         new event(2, 'event 4', new Date(2024, 2, 20, 11), new Date(2024, 2, 20, 14, 30), null, 'description', 'catagory', 'location')]);
+
     const [activeDate, setActiveDate] = useState(new Date());
+
     let eventsInMonth = []
+
     console.log(activeDate)
     for (let e of events) {
         if (e.begin_date.getFullYear() >= activeDate.getFullYear() && e.end_date.getFullYear() <= activeDate.getFullYear() &&
@@ -25,6 +26,21 @@ export default function Calender() {
         return  x.getFullYear() >= a.getFullYear() &&  x.getFullYear() <= b.getFullYear() &&
                 x.getMonth() >= a.getMonth() && x.getMonth() <= b.getMonth()  &&
                 x.getDate() >= a.getDate() && x.getDate() <= b.getDate()
+    }
+
+    function changeDate(i) {
+        let newDate = new Date(activeDate.getTime())
+        newDate.setMonth(activeDate.getMonth() + i)
+        console.log(newDate)
+        setActiveDate(newDate);
+    }
+
+    function dateForward() {
+        changeDate(1);
+    }
+
+    function dateBackward() {
+        changeDate(-1);
     }
 
     function DayStyling(date, view) {
@@ -56,22 +72,33 @@ export default function Calender() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <Calendar
-                    onChange={onChange}
-                    onActiveStartDateChange={({ action, activeStartDate, value, view }) => setActiveDate(activeStartDate)}
-                    value={value}
-                    tileClassName={({ date, view }) => DayStyling(date, view)}
-                    showNeighboringMonth={false}
-                    showFixedNumberOfWeeks={false}
-                    maxDetail={"month"}
-                />
+                {activeDate.toLocaleString('default', { year: 'numeric', month: 'long' })}
+                <div className={'flex flex-row'}>
+                    <button className={"bg-slate-900"} onClick={dateBackward}>left</button>
+                    <Calendar
+                        showNavigation={false}
+                        activeStartDate={activeDate}
+                        tileClassName={({date, view}) => DayStyling(date, view)}
+                        showNeighboringMonth={false}
+                        showFixedNumberOfWeeks={false}
+                        maxDetail={"month"}
+                    />
+                    <button className={"bg-slate-900"} onClick={dateForward}>right</button>
+                </div>
                 {eventsInMonth.map((e) =>
                     <div>
                         <div>{e.name} <span className={'text-sm text-slate-500'}>{e.location}</span></div>
+                        <span className={'text-sm text-slate-700'}>
+                            <span className={'mr-1'}>{e.begin_date.toLocaleDateString('default', {day: 'numeric'})}</span>
+                            <span className={'mr-1'}>{e.begin_date.toLocaleTimeString('default', { hour: 'numeric', minute: 'numeric'})}</span>
+                            <span className={'mr-1'}>-</span>
+                            <span className={'mr-1'}>{e.end_date.toLocaleDateString('default', {day: 'numeric'})}</span>
+                            <span className={'mr-1'}>{e.end_date.toLocaleTimeString('default', {hour: 'numeric', minute: 'numeric'})}</span>
+                        </span>
                         <div>{e.description}</div>
                     </div>)
                 }
-                {value.getMonth()}
+
             </div>
         </main>
     );
