cb28f6b7a2b5822004873570bd67e56f15969994
Ninty <ninty0808@gmail.com>
2024-03-11 09:59:22 +0100

    split up layout into header and footer

--- src/app/header.js
+++ src/app/header.js
@@ -1,10 +1,27 @@
 'use client'
 
+import {Fragment} from "react";
+import Head from "next/head";
+
 export default function Header(){
 
     return(
-        <div>
-            e
-        </div>
+            <div className="w-screen h-[100px] bg-lands-accent flex flex-row justify-center">
+                <div className="ml-[200px] w-1/3 flex flex-row justify-start">
+                    <div className="mr-[50px] h-[125px] w-[250px] bg-white">
+                        <a></a>
+                    </div>
+                </div>
+                <div className="w-1/3 flex flex-row justify-start">
+                    <ul className="mt-[35px] text-lg text-white flex flex-row space-x-10">
+                        <li className="hover:italic hover:font-bold"><a href="#">Home</a></li>
+                        <li className="hover:italic hover:font-bold"><a href="/calender">Events</a></li>
+                        <li className="hover:italic hover:font-bold"><a href="#">Fotoalbums</a></li>
+                        <li className="hover:italic hover:font-bold"><a href="#">Nieuws</a></li>
+                    </ul>
+                </div>
+                <div className="w-1/3">
+                </div>
+            </div>
     )
 }
