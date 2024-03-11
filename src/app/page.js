0618f791e7c3c83ebf1e3f62d6f3562a0f4a9b0b
ZroTwo <pizzaworld112@gmail.com>
2024-03-11 13:29:39 +0100

    homepsge changes

--- src/app/page.js
+++ src/app/page.js
@@ -17,11 +17,11 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <section className="banner">
-          <img src={"/src/img/Screenshot (103).png"} className="h-auto max-w-full" alt="image description"/>
+          <Image width={1000} height={1000} src={'/images/screenshot (103).png'} className="" alt="image description"/>
         </section>
 
         <section className="upcoming-events">
-          Opkomende evenementen
+         <h1 className=" flex justify-center pt-6 pb-6 text-xl font-bold"> Opkomende evenementen</h1>
           <section className="event-cards flex flex-row">
             <div className="pr-7">
               <div
@@ -31,6 +31,7 @@ export default function Home() {
                 </a>
                 <div className="p-5">
                   <a href="#">
+                    <Image width={1000} height={1000} src={'/images/screenshot (103).png'} className="" alt="image description"/>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
                       technology acquisitions 2021</h5>
                   </a>
@@ -47,6 +48,7 @@ export default function Home() {
                 </a>
                 <div className="p-5">
                   <a href="#">
+                    <Image width={1000} height={1000} src={'/images/screenshot (103).png'} className="" alt="image description"/>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
                       technology acquisitions 2021</h5>
                   </a>
@@ -63,6 +65,7 @@ export default function Home() {
                 </a>
                 <div className="p-5">
                   <a href="#">
+                    <Image width={1000} height={1000} src={'/images/screenshot (103).png'} className="" alt="image description"/>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
                       technology acquisitions 2021</h5>
                   </a>
@@ -74,7 +77,7 @@ export default function Home() {
           </section>
           <div className=" flex justify-center  pt-4">
             <button type="button"
-                    className=" focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Bekijk
+                    className=" focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Bekijk
               Meer
             </button>
           </div>
@@ -106,7 +109,7 @@ export default function Home() {
                       className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                       data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
                       aria-controls="accordion-collapse-body-1">
-                <span>What is Flowbite?</span>
+                <span>Zijn de evenementen op de website alleen voor studenten of kunnen anderen ook deelnemen?</span>
                 <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                   <path stroke={"currentColor"} strokeLinecap={"round"}  strokeLinejoin={"round"} strokeWidth={"2"}
@@ -129,7 +132,7 @@ export default function Home() {
                       className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                       data-accordion-target="#accordion-collapse-body-2" aria-expanded="false"
                       aria-controls="accordion-collapse-body-2">
-                <span>Is there a Figma file available?</span>
+                <span>Hoe kan ik meer informatie krijgen over een specifiek evenement dat op de website wordt vermeld?</span>
                 <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -147,6 +150,68 @@ export default function Home() {
               </div>
             </div>
             <h2 id="accordion-collapse-heading-3">
+              <button type="button"
+                      className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                      data-accordion-target="#accordion-collapse-body-3" aria-expanded="false"
+                      aria-controls="accordion-collapse-body-3">
+                <span>Worden er ook online evenementen of activiteiten op de website geplaatst, of zijn het alleen fysieke bijeenkomsten?</span>
+                <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5 5 1 1 5"/>
+                </svg>
+              </button>
+            </h2>
+            <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
+              <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components
+                  from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another
+                  difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers
+                  sections of pages.</p>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite,
+                  Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of
+                  two worlds.</p>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
+                  <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite
+                    Pro</a></li>
+                  <li><a href="https://tailwindui.com/" rel="nofollow"
+                         className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                </ul>
+              </div>
+              l
+            </div>            <h2 id="accordion-collapse-heading-3">
+              <button type="button"
+                      className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                      data-accordion-target="#accordion-collapse-body-3" aria-expanded="false"
+                      aria-controls="accordion-collapse-body-3">
+                <span>Worden er naast nieuwsberichten over evenementen ook andere soorten nieuws gepubliceerd op de website?</span>
+                <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5 5 1 1 5"/>
+                </svg>
+              </button>
+            </h2>
+            <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
+              <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components
+                  from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another
+                  difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers
+                  sections of pages.</p>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite,
+                  Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of
+                  two worlds.</p>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
+                  <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite
+                    Pro</a></li>
+                  <li><a href="https://tailwindui.com/" rel="nofollow"
+                         className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                </ul>
+              </div>
+              l
+            </div>            <h2 id="accordion-collapse-heading-3">
               <button type="button"
                       className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                       data-accordion-target="#accordion-collapse-body-3" aria-expanded="false"
